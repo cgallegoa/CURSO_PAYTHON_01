59466c5f631abf23c4e3d3d328867143e6f5ec70
cgallegoa <cgallegoa@mastersoft3000.com>
2022-11-10 16:39:28 -0500

    Create FiguraGeometrica.py

---
+++ FIGURA_GEOMETRICA/FiguraGeometrica.py
@@ -0,0 +1,38 @@
+
+from abc import ABD, abstractmethod
+
+class FiguraGeometrica:
+    def __init__(self, ancho, alto):
+        if self._validar_valor(ancho):
+          self._ancho = ancho
+        else
+          self._ancho = 0
+          print('Error, el ancho de la figura no puede ser negativo')
+
+        if self._validar_valor(alto):
+          self._alto = alto
+        else
+          self._alto = alto
+          print('Error, el alto de la figura no puede ser negativo')
+
+    def set_ancho(self, ancho):
+        self._ancho = ancho
+
+    def get_ancho(self):
+        returm self._ancho
+
+    def set_alto(self, alto):
+        self._alto = alto
+
+    def get_alto(self):
+        returm self._alto
+
+    def _validar_valor(self, valor):
+        return True if valor >= 0 else False
+
+    @abstractmethod
+    def calcular_area(self):
+        pass
+
+    def __str__(self) -> str:
+       return f'Figura geométrica [Ancho: {self._ancho}, Alto: {self._alto}]'
